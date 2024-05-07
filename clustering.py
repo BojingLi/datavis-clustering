dbf3beb49e9975754018b4a689df57d980ce9787
BojingLi <libojing@mails.ccnu.edu.cn>
2024-05-07 16:58:24 -0400

    1
    1

---
+++ clustering.py
@@ -0,0 +1,47 @@
+
+
+import pandas as pd
+
+
+
+def readDatabyState(stateName):
+    df = pd.read_excel('Patent by state and category.xlsx', sheet_name=stateName)
+    return df
+
+
+stateList = ['Alabama','Alaska']
+
+
+# stateList = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware',
+#              'Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky',
+#              'Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri',
+#              'Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina',
+#              'North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina',
+#              'South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia',
+#              'Wisconsin','Wyoming','District of Columbia']
+
+for state in stateList:
+    locals()[state] = readDatabyState(state)
+    # df = readDatabyState(state)
+
+
+
+
+
+
+print('end')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
